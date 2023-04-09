a29a52e8fd50c4bdfeb8cee70869b312dbdd62b9
sapiwaidi <sapikwaidi@gmail.com>
2023-04-09 18:56:47 +0700

    git age v 2

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("buttonAge").addEventListener("click", function(){
+    var tgl1=new Date(document.getElementById("yourbirth").value);
+    var tgl2=new Date(); 
+    var timeDiff = Math.abs(tgl2.getTime() - tgl1.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+    document.getElementById("yourAge").innerHTML = Math.round(diffDays/365) + " Tahun";
+});
